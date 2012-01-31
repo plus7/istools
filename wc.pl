f5219792e4bed328dfb9a98b60c1c88af9fc185b
NOSE Takafumi <tknose@is.s.u-tokyo.ac.jp>
2012-02-01 05:01:40 +0900

    word count script

---
+++ wc.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w
+
+open( OUT, "| wc -w" );
+while ( $#ARGV >= 0 ) {
+    my $file = shift @ARGV;
+
+    #  print "$file\n";
+    open( IN, "<$file" );
+    while (<IN>) {
+        if ( /begin/ && /jabstract/ ) {
+            do { $_ = <IN>; } while ( !( /end/ && /jabstract/ ) );
+        }
+        s/\%.*//g;
+        s/\\textit\{(.*?)\}/$1/g;
+        s/\\footnote\{(.*?)\}/$1/g;
+        s/\\includegraphics\[.*?\]\{.*?\}//g; # includegraphicsのオプション指定部に半角スペースがあるとおかしくなるのを直した 2012/01/30
+        s/\\[^\s\{]*\{[^\}]*\}//g;    # \begin{center}
+        s/\\[^\s]*//g;                # \alpha
+        s/\[[^\]]*\]//g;              # \begin{figure}[htb]
+        s/\_[^\s]+//g;
+        s/\^[^\s]+//g;
+        s/[:\!\~]+//g;
+        s/[\{\}\&\$\-\+\(\)=<>\.\,\[\]]+/ /g;
+        print OUT;
+    }
+    close(IN);
+}
+close(OUT);
